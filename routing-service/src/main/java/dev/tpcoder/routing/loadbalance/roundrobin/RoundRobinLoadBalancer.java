e968d80ec7a4e251865bd1311c9a5ef9bb86a748
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-09-10 14:21:40 +0900

    Apply timeout request and consider any timeout as failure counting

--- routing-service/src/main/java/dev/tpcoder/routing/loadbalance/roundrobin/RoundRobinLoadBalancer.java
+++ routing-service/src/main/java/dev/tpcoder/routing/loadbalance/roundrobin/RoundRobinLoadBalancer.java
@@ -5,9 +5,11 @@ import dev.tpcoder.routing.loadbalance.TrafficDistributable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.time.Duration;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -24,7 +26,10 @@ public class RoundRobinLoadBalancer implements TrafficDistributable {
             RestClient.Builder restClientBuilder
     ) {
         this.roundRobinConfiguration = roundRobinConfiguration;
-        this.restClient = restClientBuilder.build();
+        var timeoutMs = roundRobinConfiguration.getRoutingProperties().getTimeout().toMillis();
+        this.restClient = restClientBuilder
+                .requestFactory(createClientHttpRequestFactory(timeoutMs))
+                .build();
     }
 
     @Override
@@ -34,17 +39,20 @@ public class RoundRobinLoadBalancer implements TrafficDistributable {
         if (hostsList.isEmpty() || isAllHostsUnhealthy(healthStatusMap)) {
             throw new RuntimeException("No healthy hosts available");
         }
-        var threshold = roundRobinConfiguration.getRoutingProperties().getFailureAttemptThreshold();
-        // Retry by using attempt
-        while (threshold > 0) {
+
+        var routingProperties = roundRobinConfiguration.getRoutingProperties();
+        var failureThreshold = routingProperties.getFailureAttemptThreshold();
+
+        // Retry using failure attempt threshold
+        while (failureThreshold > 0) {
             String selectedHost = getNextRoundRobinHost(hostsList);
             if (healthStatusMap.get(selectedHost)) {
                 try {
                     return upstreamRequest(body, selectedHost);
                 } catch (Exception e) {
                     logger.error("Failed to route request to host: {}", selectedHost, e);
-                    threshold--;
-                    logger.debug("Retry remaining: {}", threshold);
+                    failureThreshold--;
+                    logger.debug("Retry remaining: {}", failureThreshold);
                 }
             }
         }
@@ -70,6 +78,13 @@ public class RoundRobinLoadBalancer implements TrafficDistributable {
 
     private boolean isAllHostsUnhealthy(ConcurrentHashMap<String, Boolean> hostHealthStatusMap) {
         return hostHealthStatusMap.values().stream().noneMatch(Boolean.TRUE::equals);
+    }
+
+    private HttpComponentsClientHttpRequestFactory createClientHttpRequestFactory(long timeoutMs) {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(Duration.ofMillis(timeoutMs));
+        clientHttpRequestFactory.setConnectionRequestTimeout(Duration.ofMillis(timeoutMs));
+        return clientHttpRequestFactory;
     }
 
 }
