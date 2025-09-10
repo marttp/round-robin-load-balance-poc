a454acf12205174d6ffcafa692acbffa55e172f6
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-09-10 10:23:11 +0900

    Add routing properties to load in backend application

---
+++ routing-service/src/main/java/dev/tpcoder/routing/config/RoutingProperties.java
@@ -0,0 +1,48 @@
+package dev.tpcoder.routing.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class RoutingProperties {
+
+    @Value("${routing.upstream.hosts}")
+    private String hosts;
+
+    @Value("${routing.upstream.timeout}")
+    private Duration timeout;
+
+    @Value("${routing.upstream.failure-attempt-threshold}")
+    private int failureAttemptThreshold;
+
+    @Value("${routing.health-check.interval}")
+    private Duration healthCheckInterval;
+
+    private List<String> upstreamHosts;
+
+    @PostConstruct
+    public void init() {
+        this.upstreamHosts = Arrays.asList(hosts.trim().split(","));
+    }
+
+    public List<String> getHostsList() {
+        return this.upstreamHosts;
+    }
+
+    public Duration getTimeout() {
+        return timeout;
+    }
+
+    public int getFailureAttemptThreshold() {
+        return failureAttemptThreshold;
+    }
+
+    public Duration getHealthCheckInterval() {
+        return healthCheckInterval;
+    }
+}
