b837266deeb839aed29dbce4a293d927c0b332bf
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-09-10 13:36:04 +0900

    Handle the case when stale health check is appeared so using retry attempt to handle the request

--- routing-service/src/main/java/dev/tpcoder/routing/loadbalance/roundrobin/RoundRobinLoadBalancer.java
+++ routing-service/src/main/java/dev/tpcoder/routing/loadbalance/roundrobin/RoundRobinLoadBalancer.java
@@ -8,6 +8,9 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 @Service
 public class RoundRobinLoadBalancer implements TrafficDistributable {
 
@@ -27,29 +30,46 @@ public class RoundRobinLoadBalancer implements TrafficDistributable {
     @Override
     public JsonNode distributeTraffic(JsonNode body) {
         var hostsList = roundRobinConfiguration.getHostsList();
-
-        if (hostsList.isEmpty()) {
+        var healthStatusMap = roundRobinConfiguration.getHostHealthStatusMap();
+        if (hostsList.isEmpty() || isAllHostsUnhealthy(healthStatusMap)) {
             throw new RuntimeException("No healthy hosts available");
         }
+        var threshold = roundRobinConfiguration.getRoutingProperties().getFailureAttemptThreshold();
+        // Retry by using attempt
+        while (threshold > 0) {
+            String selectedHost = getNextRoundRobinHost(hostsList);
+            if (healthStatusMap.get(selectedHost)) {
+                try {
+                    return upstreamRequest(body, selectedHost);
+                } catch (Exception e) {
+                    logger.error("Failed to route request to host: {}", selectedHost, e);
+                    threshold--;
+                    logger.debug("Retry remaining: {}", threshold);
+                }
+            }
+        }
+        throw new RuntimeException("Failed to route request to hosts");
+    }
 
+    private JsonNode upstreamRequest(JsonNode body, String host) {
+        return restClient.post()
+                .uri(host)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(body)
+                .retrieve()
+                .body(JsonNode.class);
+    }
+
+    private String getNextRoundRobinHost(CopyOnWriteArrayList<String> hostsList) {
         var totalSize = hostsList.size();
         var currentIndex = roundRobinConfiguration
                 .getRoundRobinCounter()
                 .getAndUpdate(c -> (c + 1) % totalSize);
-
-        String selectedHost = hostsList.get(currentIndex);
-        logger.info("Routing request to host: {} (index: {})", selectedHost, currentIndex);
-
-        try {
-            return restClient.post()
-                    .uri(selectedHost)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(body)
-                    .retrieve()
-                    .body(JsonNode.class);
-        } catch (Exception e) {
-            logger.error("Failed to route request to host: {}", selectedHost, e);
-            throw new RuntimeException("Failed to route request to host: " + selectedHost, e);
-        }
+        return hostsList.get(currentIndex);
     }
+
+    private boolean isAllHostsUnhealthy(ConcurrentHashMap<String, Boolean> hostHealthStatusMap) {
+        return hostHealthStatusMap.values().stream().noneMatch(Boolean.TRUE::equals);
+    }
+
 }
