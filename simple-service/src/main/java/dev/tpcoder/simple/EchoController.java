c39b16dce28ade3044d6fcb40d92ec7750651064
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-09-10 11:03:52 +0900

    Add log on simple service

--- simple-service/src/main/java/dev/tpcoder/simple/EchoController.java
+++ simple-service/src/main/java/dev/tpcoder/simple/EchoController.java
@@ -1,6 +1,8 @@
 package dev.tpcoder.simple;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/echo")
 public class EchoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(EchoController.class);
+
     @PostMapping
     public JsonNode echo(@RequestBody JsonNode payload) {
+        logger.info("Echo service received request: {}", payload);
         return payload;
     }
 }
